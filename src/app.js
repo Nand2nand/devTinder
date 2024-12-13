375d42b59d593e85f7ed423fa72f6f0a35487844
vairamani-m <dimondbell007@gmail.com>
2024-12-13 12:42:14 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/",(req, res)=>{
+    res.send("Namaste");
+});
+
+app.use("/hello",(req, res)=>{
+    res.send("Hello hello hello");
+});
+
+app.use("/test",(req, res)=>{
+    res.send("Hello from the server");
+});
+
+app.listen(7777,()=>{
+    console.log("server is successfullly listening to 7777");
+});
